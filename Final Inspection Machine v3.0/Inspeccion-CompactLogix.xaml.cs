e5f5bb984474c3b2de1cfa1d970913fd2db9a2d3
daniel <danie@DESKTOP-8B46O8E>
2024-06-12 16:24:06 -0500

    12062416-3

--- Final Inspection Machine v3.0/Inspeccion-CompactLogix.xaml.cs
+++ Final Inspection Machine v3.0/Inspeccion-CompactLogix.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
